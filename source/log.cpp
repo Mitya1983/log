0e8a9d8b81d3946f24c4ec792b2b406da236652a
Mitia Tristan <33630878+Mitya1983@users.noreply.github.com>
2025-08-12 15:39:52 +0200

    Made default log output cross platform  log.cpp

--- source/log.cpp
+++ source/log.cpp
@@ -79,18 +79,18 @@ auto LogEvent::toString(const std::function< std::string(const LogEvent&) >& for
     return std::format("{}|{}|{}|{}|{}|{}|{}\n", time_point, message_type_string, module_name, message, function_name, file_name, line);
 #endif
     const auto time = std::chrono::system_clock::to_time_t(time_point);
-    auto [tm_sec, tm_min, tm_hour, tm_mday, tm_mon, tm_year, tm_wday, tm_yday, tm_isdst] = *std::gmtime(&time);
-    std::string string_time = std::to_string(tm_year + 1900);
+    auto tm_struct = *std::gmtime(&time);
+    std::string string_time = std::to_string(tm_struct.tm_year + 1900);
     string_time += '-';
-    string_time += std::to_string(tm_mon + 1);
+    string_time += std::to_string(tm_struct.tm_mon + 1);
     string_time += '-';
-    string_time += std::to_string(tm_mday);
+    string_time += std::to_string(tm_struct.tm_mday);
     string_time += 'T';
-    string_time += std::to_string(tm_hour);
+    string_time += std::to_string(tm_struct.tm_hour);
     string_time += ':';
-    string_time += std::to_string(tm_min);
+    string_time += std::to_string(tm_struct.tm_min);
     string_time += ':';
-    string_time += std::to_string(tm_sec);
+    string_time += std::to_string(tm_struct.tm_sec);
     return {string_time + " | " + message_type_string + " | " + module_name + " | " + message + " | " + function_name + " | " + file_name + " | " + line
             + '\n'};
 }
