95e338afeb74667cbe13116472b829690657c873
mitia <mitia.tristan.it@pm.me>
2026-03-06 23:47:16 +0100

    Fixed issues

--- include/log.hpp
+++ include/log.hpp
@@ -34,14 +34,14 @@ namespace mt::log {
     struct log_event {
         log_event(std::string p_module,
                   std::string p_message,
-                  message_type p_message_type,
+                  enum message_type p_message_type,
                   std::string p_function_name,
                   std::string p_file_name,
                   uint32_t p_line);
         log_event(std::string p_module, std::string p_message, enum message_type p_message_type, std::source_location p_source_location);
         log_event(std::string_view p_module,
                   std::string_view p_message,
-                  message_type p_message_type,
+                  enum message_type p_message_type,
                   std::string p_function_name,
                   std::string p_file_name,
                   uint32_t p_line);
